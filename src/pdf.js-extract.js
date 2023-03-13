0a38297dd36aba5e34713bd0d635f30956137dc5
Denisio Rordrigues <denisio@ymail.com>
2023-03-13 07:30:24 -0300

    changing extract from pdf.js-extract

--- src/pdf.js-extract.js
+++ src/pdf.js-extract.js
@@ -3,12 +3,15 @@ const pdfExtract = new PDFExtract();
 const fs = require('fs');
 const buffer = fs.readFileSync("src/example.pdf");
 const options = {}; /* see below */
+
 pdfExtract.extractBuffer(buffer, options, (err, data) => {
   if (err) return console.log(err);
-//   console.log(data);
-  console.log(data.pages[0].content);
-
-    // const { contents } = data.pages;
-    // console.log(data.pages);
-
+    //console.log(data.pages[0].content);
+    fs.writeFile('pdf.js-extract_1.txt', JSON.stringify(data), err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('File saved successfully!');
+    });
 }); 
